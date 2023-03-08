170c34849dcb042d207655306d4b3bf470143485
lekishonsheila <lekishonsheila@gmail.com>
2023-03-08 19:58:15 +0300

    new changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,10 @@ var car= Car("Subaru","Legacy","White",30)
     car.identity()
     val parkingFees = car.calculateParkingFees(4)
     println("Parking fees: $parkingFees")
-
-
-
+    var bus = Bus("Nissan", "typec456","red",80)
+    bus.maxTripFare(arrayOf(400.0,350.50,600.40,500.0,750.0))
+   println(bus.maxTripFare(arrayOf(400.0,350.50,600.40,500.0,750.0)))
+    println(bus.calculateParkingFees(2))
 
 }
 open class Car(var make:String, var model:String, var color:String, var capacity:Int){
@@ -22,17 +23,23 @@ open class Car(var make:String, var model:String, var color:String, var capacity
     fun identity() {
         println("I am a $color $make $model")
     }
-    fun calculateParkingFees(hours: Int): Int {
+    open fun calculateParkingFees(hours: Int): Int {
         return hours * 20
     }
 
 }
-class bus(make:String, model:String, color:String, capacity:Int):Car(make, model, color, capacity) {
+class Bus(make:String, model:String, color:String, capacity:Int):Car(make, model, color, capacity) {
     fun maxTripFare(fare: Array<Double>): Double {
         return fare.max()
     }
 
+    override fun calculateParkingFees(hours: Int): Int {
+        var parkingFees = hours * capacity
+        return parkingFees
+    }
 }
+
+
 
 
 
